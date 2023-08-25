a7e7f1c2fb2ba5b2616f436b1452e9223bac3e45
Karthik Durai <karthik@example.com>
2023-08-25 01:25:30 -0400

    Added path module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 const connectMongoDB = require("./config/dbConnection");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
